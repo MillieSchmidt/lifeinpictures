4846140aadc466aab031a1eb2a7c5c11d91b79cc
Millie Schmidt <millenium2124@gmail.com>
2021-07-12 15:44:53 +0300

    changed path name of album

--- src/assets/telaviv.js
+++ src/assets/telaviv.js
@@ -2,105 +2,105 @@ export const TELAVIV = [
     {
         id: 0,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture0.jpg',
+        src: 'assets/images/telavivjourney/picture0.jpg',
         alt: 'דגל בחלון',
         caption: 'אל תדרוך עליי, בדירה של אנה בתל אביב'
     },
     {
         id: 1,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture1.jpg',
+        src: 'assets/images/telavivjourney/picture1.jpg',
         alt: 'אנה',
         caption: 'הנה אנה!'
     },
     {
         id: 2,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture2.jpg',
+        src: 'assets/images/telavivjourney/picture2.jpg',
         alt: 'צוקי החתול',
         caption: 'צוקי החתול, פעם הוריד לי את אחת מאצבעות הרגליים'
     },
     {
         id: 3,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture3.jpg',
+        src: 'assets/images/telavivjourney/picture3.jpg',
         alt: 'אנה מדברת',
         caption: 'לא זוכר על מה דיברנו'
     },
     {
         id: 4,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture4.jpg',
+        src: 'assets/images/telavivjourney/picture4.jpg',
         alt: 'עץ בפארק',
         caption: 'הפארק ברחוב קפלן בתל אביב'
     },
     {
         id: 5,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture5.jpg',
+        src: 'assets/images/telavivjourney/picture5.jpg',
         alt: 'מכוניות על רחוב קפלן',
         caption: 'רחוב קפלן מחבר את עזריאלי לדיזנגוף סנטר, זה המסלול הקבוע שלי'
     },
     {
         id: 6,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture6.jpg',
+        src: 'assets/images/telavivjourney/picture6.jpg',
         alt: 'מכוניות נוסעות בכביש',
         caption: 'מכוניות נוסעות לכל הכיוונים'
     },
     {
         id: 7,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture7.jpg',
+        src: 'assets/images/telavivjourney/picture7.jpg',
         alt: 'צומת אבן גבירול',
         caption: 'לפעמים יש רגעים של שקט בכבישי תל אביב, כמו בצומת אבן גבירול הזאת'
     },
     {
         id: 8,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture8.jpg',
+        src: 'assets/images/telavivjourney/picture8.jpg',
         alt: 'נוף בדרום תל אביב',
         caption: 'דרום תל אביב מהחלון'
     },
     {
         id: 9,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture9.jpg',
+        src: 'assets/images/telavivjourney/picture9.jpg',
         alt: 'חניון בדרום תל אביב',
         caption: 'נוף מעניין מאוד יש בחלון הזה'
     },
     {
         id: 10,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture91.jpg',
+        src: 'assets/images/telavivjourney/picture91.jpg',
         alt: 'יעל שוכבת',
         caption: 'הלכתי להתארח אצל יעל'
     },
     {
         id: 11,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture92.jpg',
+        src: 'assets/images/telavivjourney/picture92.jpg',
         alt: 'יעל',
         caption: 'תודה, יעלי!'
     },
     {
         id: 12,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture93.jpg',
+        src: 'assets/images/telavivjourney/picture93.jpg',
         alt: 'פירוק בשר באטליז',
         caption: 'יום למחרת הלכתי לראות את ליה מפרקת בשר באטליז'
     },
     {
         id: 13,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture94.jpg',
+        src: 'assets/images/telavivjourney/picture94.jpg',
         alt: 'יואב איתמר',
         caption: 'באותו שוק היה גם יואב'
     },
     {
         id: 14,
         path: 'tel-aviv-walk',
-        src: 'assets/images/tel aviv journey/picture95.jpg',
+        src: 'assets/images/telavivjourney/picture95.jpg',
         alt: 'יעל גורן',
         caption: 'גם זו יעל, אבל אותה נדיר יותר לפגוש, ואי אפשר לדעת באיזו עיר'
     }
