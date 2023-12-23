562af0da0542f66791aa4a59506300cc5e67da06
Millie Schmidt <65649134+MillieSchmidt@users.noreply.github.com>
2023-12-23 18:22:49 +0200

    added ScrollToTop to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './views/Main';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop>
         <div>
           <Main />
         </div>
+      </ScrollToTop>
     </Router>
   );
 }
